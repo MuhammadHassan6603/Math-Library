f81bc95c023d3a526f4f9c244812e4d669ad5cd6
Muhammad Hassan <105521572+MuhammadHassan6603@users.noreply.github.com>
2022-07-02 22:22:53 +0500

    Add files via upload

---
+++ Hi.java
@@ -0,0 +1,16 @@
+import java.lang.Math;
+import java.lang.String;
+public class Hi
+{
+    public static void main(String[] args)
+    {
+        int i=10;
+        int j=20;
+        System.out.println("Min Value = " +Math.min(i,j));
+        System.out.println("Max Value = " +Math.max(i,j));
+        System.out.println("Power of i into 2 = " +(int)Math.pow(i,2));
+        System.out.println("Power of j into 2 = " +(int)Math.pow(j,2));
+        System.out.println("Log of i = "  +Math.log(i));
+        System.out.println("Log of j = " +Math.log(j));
+    }
+}
